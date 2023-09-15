2933c92234d08ce0b7cd074064c54a3fd9be8ac3
Austin J. Baird <90073330+galuxes@users.noreply.github.com>
2023-09-15 15:44:11 -0400

    passed all tests!!!!

--- Assets/Scripts/Integrator.cs
+++ Assets/Scripts/Integrator.cs
@@ -6,11 +6,11 @@ public static class Integrator
 {
     public static void Integrate(Particle2D particle, float dt)
     {
-        particle.velocity += particle.acceleration * dt;
-        
         particle.transform.position += (Vector3)particle.velocity * dt;
-
+        
         particle.velocity *= Mathf.Pow(particle.damping, dt);
+        
+        particle.velocity += particle.acceleration * dt;
     }
 }
 
--- Assets/Scripts/Particle2D.cs
+++ Assets/Scripts/Particle2D.cs
@@ -3,12 +3,11 @@ using UnityEngine;
 public class Particle2D : MonoBehaviour
 {
     public float mass, inverseMass;
-    public Vector2 velocity, acceleration = Vector2.zero, accumulatedForces = Vector2.zero, accelerationDueToGravity = new Vector2(0f,-9.8f);
+    public Vector2 velocity, acceleration = Vector2.zero, accumulatedForces = Vector2.zero, accelerationDueToGravity;
     [Range(0.00f, 1f)] public float damping = 1f;
 
     private void FixedUpdate()
     {
-        acceleration = accelerationDueToGravity;
         Integrator.Integrate(this, Time.deltaTime);
     }
 }
